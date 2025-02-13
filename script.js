98364a1efac05d9d8dd944c6646b314e3a5b60dc
jasonsaba1209 <60962513+jasonsaba1209@users.noreply.github.com>
2025-02-13 20:39:28 +0100

    Update script.js
    -

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function loadData() {
     Promise.all([
         fetch('output.json').then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok: ' + response.statusText);
+                throw new Error('Network response was not ok: ' + response.statusText); 
             }
             return response.json();
         }),
